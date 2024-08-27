932efbdd04f87473c8a4e684dbbcf61f30d73f51
Roman15122022 <malyir33@gmail.com>
2024-08-27 18:26:45 +0300

    refactor: change directories

--- src/screens/Popup/index.tsx
+++ src/screens/Popup/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import '../assets/tailwind.css'
+import '../../assets/tailwind.css'
 import Popup from "./popup";
 
 function init() {
--- src/screens/Popup/popup.tsx
+++ src/screens/Popup/popup.tsx
@@ -4,7 +4,7 @@ import './popup.css'
 const Popup = () => {
   return (
     <div>
-      <h1 className="text-4xl text-green-500">Hello World</h1>
+      <h1 className="text-2xl text-center">Track your time!</h1>
     </div>
   )
 };
