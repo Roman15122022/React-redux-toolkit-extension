f2f297d1ea83bd429521dc78e0d679087b6d0864
Roman15122022 <malyir33@gmail.com>
2024-08-27 17:36:43 +0300

    refactor: structure

--- src/tabs/tabs.tsx
+++ src/tabs/tabs.tsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import './tabs.css'
-import { Link } from 'react-router-dom'
+import {Link, Route, Routes} from 'react-router-dom'
+import Home from './components/Home/Home'
+import About from './components/About/About'
 
 function Tabs() {
-    return (
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </div>
-    )
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </div>
+  )
 }
 
 export default Tabs
