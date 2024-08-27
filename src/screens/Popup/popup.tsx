75e12e5cd5ed96b264fba98c55c627a7021291cc
Roman15122022 <malyir33@gmail.com>
2024-08-28 01:33:28 +0300

    feat: add hooks

--- src/screens/Popup/popup.tsx
+++ src/screens/Popup/popup.tsx
@@ -4,7 +4,7 @@ import './popup.css'
 const Popup = () => {
   return (
     <div>
-      <h1 className="text-2xl text-center">Track your time!</h1>
+      <h1 className="text-2xl font-semibold text-center my-2">Track your time!</h1>
     </div>
   )
 };
