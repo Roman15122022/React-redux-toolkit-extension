7b4fc49d0e654a0a5a165c1b84d2a50ccf052c44
Roman15122022 <malyir33@gmail.com>
2024-09-06 01:08:00 +0300

    fix: build

--- src/hooks/useTranslate/index.ts
+++ src/hooks/useTranslate/index.ts
@@ -4,14 +4,16 @@ import { useAppDispatch } from '../useAppDispatch'
 import { settingSlice } from '../../store/reducers/settingReducer/SettingSlice'
 import { TRANSLATIONS } from './constants'
 import { Language } from '../../types'
-import { Locale } from "../../types";
+import { Locale } from '../../types'
 
 export const useTranslate = () => {
   const { language } = useAppSelector(state => state.SettingReducer)
   const dispatch = useAppDispatch()
   const { setLocale } = settingSlice.actions
 
-  const [interfaceLang, setInterfaceLang] = useState<Locale>(TRANSLATIONS[language])
+  const [interfaceLang, setInterfaceLang] = useState<Locale>(
+    TRANSLATIONS[language],
+  )
 
   function handleChangeLocale(locale: Language) {
     dispatch(setLocale(locale))
