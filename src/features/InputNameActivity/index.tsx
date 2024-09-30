0dbedd71fbf50551c9c25670751f3203f624dd46
Roman15122022 <malyir33@gmail.com>
2024-10-01 00:03:42 +0300

    fix: design input 24-add-achievements-1000-hours-10000-hours

--- src/features/InputNameActivity/index.tsx
+++ src/features/InputNameActivity/index.tsx
@@ -8,7 +8,7 @@ import { InputNameActivityProps } from './types'
 const InputNameActivity = ({
   nameLabel,
 }: InputNameActivityProps): JSX.Element => {
-  const { name } = useInputNameActivity()
+  const { defaultColor, activeColor, textColor } = useInputNameActivity()
 
   return (
     <Autocomplete
@@ -18,33 +18,38 @@ const InputNameActivity = ({
         <TextField
           {...params}
           label={nameLabel}
+          error={false}
           sx={{
-            width: 150,
+            width: 160,
             height: 39,
             padding: 0,
             '& .MuiOutlinedInput-notchedOutline': {
-              borderColor: 'purple', // Color outline
+              borderColor: `${defaultColor} !important`, // Color outline
             },
             '&:hover .MuiOutlinedInput-notchedOutline': {
-              borderColor: 'darkpurple', // On hover outline color
+              borderColor: `${activeColor} !important`, // On hover outline color
+              color: 'red',
             },
             '& .MuiInputLabel-root': {
-              color: 'white',
+              color: defaultColor,
               marginLeft: '3px',
               fontSize: '13px',
             },
             '& .MuiInputLabel-root.Mui-focused': {
-              color: 'purple', //  Color label focus
+              color: activeColor, //  Color label focus
               marginLeft: '3px',
             },
             '& .MuiInputBase-input': {
-              color: 'white', // Color Text
+              color: textColor, // Color Text
               padding: '4.5px 4px 3.5px 5px !important',
               marginBottom: '3px',
             },
             '& .MuiOutlinedInput-root': {
-              padding: '7px !important', // Применяем padding через sx
+              padding: '7px !important',
               borderRadius: '10px',
+            },
+            '& .MuiSvgIcon-root': {
+              fill: activeColor,
             },
           }}
         />
--- src/features/InputNameActivity/useInputNameActivity.ts
+++ src/features/InputNameActivity/useInputNameActivity.ts
@@ -1,7 +1,14 @@
+import { ThemeVariants } from '../../types'
 import { useAppSelector } from '../../hooks/useAppSelector'
 
 export const useInputNameActivity = () => {
   const { theme } = useAppSelector(state => state.SettingReducer)
 
-  return { name: 1 }
+  const isDark = theme === ThemeVariants.DARK
+
+  return {
+    defaultColor: isDark ? '#631870' : '#ff6347',
+    activeColor: isDark ? '#ab2bc0' : '#ff6347',
+    textColor: isDark ? 'white' : 'black',
+  }
 }
