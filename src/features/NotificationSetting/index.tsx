26f3eed967386f68829ddbdf7d635184789dc041
Roman15122022 <malyir33@gmail.com>
2024-10-25 00:45:03 +0300

    feat: UI for setting 35-implement-statistic

--- src/features/NotificationSetting/index.tsx
+++ src/features/NotificationSetting/index.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import TextField from '@mui/material/TextField'
 import { Switch, Tooltip } from '@mui/material'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import DoneIcon from '@mui/icons-material/Done'
+import ClearIcon from '@mui/icons-material/Clear'
 
 import { TypeTittle } from '../../types'
 import Title from '../../components/Title'
+import Button from '../../components/Button'
 
 import { useNotificationSetting } from './useNotificationSetting'
 import { NotificationSettingProps } from './types'
@@ -14,8 +17,20 @@ const NotificationSetting = ({
   interfaceLang,
   isDark,
 }: NotificationSettingProps): JSX.Element => {
-  const { isNotificationOn, handleSetIsOnNotification } =
-    useNotificationSetting()
+  const {
+    isNotificationOn,
+    handleSetIsOnNotification,
+    handleChangePeriod,
+    handleOnChangingMode,
+    handleCancelChangingMode,
+    handleSubmitNewPeriod,
+    newPeriod,
+    styles,
+    isError,
+    isChangingMode,
+    startPeriod,
+    errorText,
+  } = useNotificationSetting(isDark, interfaceLang)
 
   return (
     <div className="my-4">
@@ -46,13 +61,43 @@ const NotificationSetting = ({
             title={interfaceLang.settings.notification.period}
             variant={TypeTittle.SMALL}
           />
-          <div className="flex items-center gap-3">
-            <TextField label="Number" type="number" />
-            <Title
-              title={interfaceLang.settings.notification.minutes}
-              variant={TypeTittle.SMALL}
-            />
-          </div>
+          {isChangingMode ? (
+            <div className="flex items-center gap-3 py-4 relative">
+              <div className="flex flex-col">
+                <TextField
+                  autoFocus={isChangingMode}
+                  label={interfaceLang.settings.notification.labelInput}
+                  error={isError}
+                  sx={styles}
+                  onChange={handleChangePeriod}
+                  value={newPeriod}
+                />
+                {isError && errorText && (
+                  <span className="font-extrabold text-[8px] text-red-600 absolute -bottom-1.5">
+                    {errorText}
+                  </span>
+                )}
+              </div>
+
+              <Button
+                classes="mt-[10px] py-1"
+                onClick={handleCancelChangingMode}
+              >
+                <ClearIcon sx={{ fontSize: 13 }} />
+              </Button>
+              <Button
+                disabled={isError}
+                classes="mt-[10px] py-1"
+                onClick={handleSubmitNewPeriod}
+              >
+                <DoneIcon sx={{ fontSize: 13 }} />
+              </Button>
+            </div>
+          ) : (
+            <Button onClick={handleOnChangingMode}>
+              {startPeriod} {interfaceLang.settings.notification.minutes}
+            </Button>
+          )}
         </div>
       )}
     </div>
--- src/features/NotificationSetting/useNotificationSetting.ts
+++ src/features/NotificationSetting/useNotificationSetting.ts
@@ -1,10 +1,15 @@
 import { ChangeEvent, useState } from 'react'
 
+import { Locale } from '../../types'
 import { settingSlice } from '../../store/reducers/settingReducer/SettingSlice'
 import { useAppSelector } from '../../hooks/useAppSelector'
 import { useAppDispatch } from '../../hooks/useAppDispatch'
+import { TIME_IN_SECONDS } from '../../constants'
 
-export const useNotificationSetting = () => {
+export const useNotificationSetting = (
+  isDark: boolean,
+  interfaceLang: Locale,
+) => {
   const dispatch = useAppDispatch()
   const { notification } = useAppSelector(state => state.SettingReducer)
 
@@ -13,9 +18,13 @@ export const useNotificationSetting = () => {
   const [isNotificationOn, setIsNotificationOn] = useState<boolean>(
     notification?.isNotificationActive ?? true,
   )
-  const [period, setPeriod] = useState<number>(
+  const [startPeriod, setStartPeriod] = useState<number>(
     notification?.periodInMinutes ?? 60,
   )
+  const [newPeriod, setNewPeriod] = useState<number>(startPeriod)
+  const [isError, setIsError] = useState<boolean>(false)
+  const [isChangingMode, setIsChangingMode] = useState<boolean>(false)
+  const [errorText, setErrorText] = useState<string>('')
 
   const handleSetIsOnNotification = (
     event: ChangeEvent<HTMLInputElement>,
@@ -26,16 +35,109 @@ export const useNotificationSetting = () => {
     dispatch(
       setNotification({
         isNotificationActive: isNotification,
-        periodInMinutes: period,
+        periodInMinutes: newPeriod,
       }),
     )
   }
 
-  const handleChangePeriod = (period: number): void => {
-    if (typeof period !== 'number') return
+  const handleChangePeriod = (event: ChangeEvent<HTMLInputElement>): void => {
+    const periodNumber = Number(event.target.value)
 
-    setPeriod(period)
+    if (Number.isNaN(periodNumber) || periodNumber > TIME_IN_SECONDS.DAY) {
+      setIsError(true)
+      setErrorText(interfaceLang.settings.notification.errorFirst)
+
+      return
+    }
+
+    if (periodNumber === 0) {
+      setNewPeriod(periodNumber)
+      setIsError(true)
+      setErrorText(interfaceLang.settings.notification.errorSecond)
+
+      return
+    }
+
+    setNewPeriod(periodNumber)
+    setIsError(false)
   }
 
-  return { isNotificationOn, handleSetIsOnNotification, handleChangePeriod }
+  const handleCancelChangingMode = (): void => {
+    setIsChangingMode(false)
+
+    setNewPeriod(startPeriod)
+    setIsError(false)
+  }
+
+  const handleOnChangingMode = (): void => {
+    setIsChangingMode(true)
+  }
+
+  const handleSubmitNewPeriod = (): void => {
+    if (isError) return
+
+    setStartPeriod(newPeriod)
+
+    dispatch(
+      setNotification({
+        isNotificationActive: isNotificationOn,
+        periodInMinutes: newPeriod,
+      }),
+    )
+
+    setIsChangingMode(false)
+  }
+
+  const defaultColor = isDark ? '#631870' : '#ff6347'
+  const activeColor = isDark ? '#ab2bc0' : '#ff6347'
+  const textColor = isDark ? 'white' : 'black'
+
+  const styles = {
+    width: 100,
+    height: 39,
+    padding: 0,
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: `${isError ? 'red' : defaultColor} !important`, // Color outline
+    },
+    '&:hover .MuiOutlinedInput-notchedOutline': {
+      borderColor: `${isError ? 'red' : activeColor} !important`, // On hover outline color
+      color: 'red',
+    },
+    '& .MuiInputLabel-root': {
+      color: `${isError ? 'red' : defaultColor}`,
+      marginLeft: '3px',
+      fontSize: '13px',
+    },
+    '& .MuiInputLabel-root.Mui-focused': {
+      color: `${isError ? 'red' : activeColor}`, //  Color label focus
+      marginLeft: '3px',
+    },
+    '& .MuiInputBase-input': {
+      color: textColor, // Color Text
+      padding: '4.5px 4px 3.5px 5px !important',
+      marginBottom: '3px',
+    },
+    '& .MuiOutlinedInput-root': {
+      padding: '7px !important',
+      borderRadius: '10px',
+    },
+    '& .MuiSvgIcon-root': {
+      fill: activeColor,
+    },
+  }
+
+  return {
+    isNotificationOn,
+    styles,
+    handleSetIsOnNotification,
+    handleChangePeriod,
+    newPeriod,
+    isError,
+    isChangingMode,
+    startPeriod,
+    handleOnChangingMode,
+    handleCancelChangingMode,
+    handleSubmitNewPeriod,
+    errorText,
+  }
 }
