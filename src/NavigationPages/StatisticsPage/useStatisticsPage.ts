c2a5afecb7245a9b4394b25d22f5003ba3dfef66
roman <mallrom1231@gmail.com>
2025-02-27 00:54:27 +0200

    feat: save state 43-improve-settings-mood-last-7-days

--- src/NavigationPages/StatisticsPage/useStatisticsPage.ts
+++ src/NavigationPages/StatisticsPage/useStatisticsPage.ts
@@ -1,18 +1,23 @@
 import { useState, useMemo } from 'react'
 import { SelectChangeEvent } from '@mui/material'
 
+import { stateSaverSlice } from '../../store/reducers/stateSaverReducer/StateSaverSlice'
 import { useTranslate } from '../../hooks/useTranslate'
 import { useStateSaver } from '../../hooks/useStateSaver'
 import { useAppSelector } from '../../hooks/useAppSelector'
+import { useAppDispatch } from '../../hooks/useAppDispatch'
 
 import { Period } from './types'
 import { getDatesByPeriod } from './helpers'
 
 export const useStatisticsPage = () => {
   const { dates } = useAppSelector(state => state.TimerLogsReducer)
-  const { isHintActive: startValueHint } = useAppSelector(
+  const { isHintActive: startValueHint, periodStat } = useAppSelector(
     state => state.StateSaverReducer,
   )
+
+  const dispatch = useAppDispatch()
+  const { savePeriod } = stateSaverSlice.actions
 
   const { interfaceLang } = useTranslate()
   const { setIsActiveHint } = useStateSaver()
@@ -20,7 +25,7 @@ export const useStatisticsPage = () => {
   const [isHintActive, setIsHintActive] = useState<boolean>(
     startValueHint ?? false,
   )
-  const [period, setPeriod] = useState<Period>('0')
+  const [period, setPeriod] = useState<Period>(periodStat || '0')
 
   const handleToggleHint = (): void => {
     setIsHintActive(prevState => {
@@ -31,7 +36,10 @@ export const useStatisticsPage = () => {
   }
 
   const handleChangePeriod = (event: SelectChangeEvent) => {
-    setPeriod(event.target.value as Period)
+    const period = event.target.value as Period
+
+    setPeriod(period)
+    dispatch(savePeriod(period))
   }
 
   const dataByPeriod = useMemo(() => {
