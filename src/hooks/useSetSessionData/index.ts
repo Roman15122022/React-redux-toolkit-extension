8ebfabf4be14e252d08a35435d6e992ec34c2959
roman <mallrom1231@gmail.com>
2025-04-08 22:41:17 +0300

    feat: lint 54-add-track-site-time

--- src/hooks/useSetSessionData/index.ts
+++ src/hooks/useSetSessionData/index.ts
@@ -1,17 +1,17 @@
-import { useAppDispatch } from "../useAppDispatch"
-import { sessionDataSlice } from "../../store/reducers/sessionDataReducer/sessionDataSlice";
-import { SessionsDomainInfo } from "../../types";
+import { useAppDispatch } from '../useAppDispatch'
+import { sessionDataSlice } from '../../store/reducers/sessionDataReducer/sessionDataSlice'
+import { SessionsDomainInfo } from '../../types'
 
 export const useSetSessionData = () => {
   const dispatch = useAppDispatch()
-  const {setSessionsData} = sessionDataSlice.actions
+  const { setSessionsData } = sessionDataSlice.actions
 
   async function getSessionData() {
-    return new Promise((resolve) => {
-      chrome.storage.local.get('sessionData', (result) => {
-        resolve(result.sessionData || []);
-      });
-    });
+    return new Promise(resolve => {
+      chrome.storage.local.get('sessionData', result => {
+        resolve(result.sessionData || [])
+      })
+    })
   }
 
   async function updateSessionData() {
