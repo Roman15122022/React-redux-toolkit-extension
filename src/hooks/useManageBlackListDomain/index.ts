df4210e7aaa0442eb08f35cd49ab6494b7e1c2c1
roman <mallrom1231@gmail.com>
2025-04-10 20:45:33 +0300

    fix: lint 54-add-track-site-time

--- src/hooks/useManageBlackListDomain/index.ts
+++ src/hooks/useManageBlackListDomain/index.ts
@@ -1,54 +1,55 @@
-import { useAppDispatch } from "../useAppDispatch";
-import { sessionDataSlice } from "../../store/reducers/sessionDataReducer/sessionDataSlice";
-import { useAppSelector } from "../useAppSelector";
+import { useAppDispatch } from '../useAppDispatch'
+import { sessionDataSlice } from '../../store/reducers/sessionDataReducer/sessionDataSlice'
+import { useAppSelector } from '../useAppSelector'
 
 export const useManageBlackListDomain = () => {
-  const dispatch = useAppDispatch();
-  const { setBlackList } = sessionDataSlice.actions;
-  const { blackList } = useAppSelector(state => state.SessionDataSlice);
+  const dispatch = useAppDispatch()
+  const { setBlackList } = sessionDataSlice.actions
+  const { blackList } = useAppSelector(state => state.SessionDataSlice)
 
   const getActualBlackList = async (): Promise<string[]> => {
     return new Promise(resolve => {
-      chrome.storage.local.get("blackList", result => {
-        resolve(result.blackList || []);
-      });
-    });
-  };
+      chrome.storage.local.get('blackList', result => {
+        resolve(result.blackList || [])
+      })
+    })
+  }
 
   const setActualBlackList = async (blackList: string[]): Promise<void> => {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       chrome.storage.local.set({ blackList }, () => {
-        resolve();
-      });
-    });
-  };
+        resolve()
+      })
+    })
+  }
 
   const handleSetBlackList = async (): Promise<void> => {
-    const blackList = await getActualBlackList();
+    const blackList = await getActualBlackList()
 
-    dispatch(setBlackList(blackList));
-  };
+    dispatch(setBlackList(blackList))
+  }
 
   const handleAddItemToBlackList = async (domain: string): Promise<void> => {
-    if (blackList.includes(domain)) return;
+    if (blackList.includes(domain)) return
 
-    const updatedBlackList = [...blackList, domain];
+    const updatedBlackList = [...blackList, domain]
 
-    dispatch(setBlackList(updatedBlackList));
-    await setActualBlackList(updatedBlackList);
+    dispatch(setBlackList(updatedBlackList))
+    await setActualBlackList(updatedBlackList)
   }
 
-  const handleRemoveItemFromBlackList = async (domain: string): Promise<void> => {
-    const updatedBlackList = blackList.filter(d => d !== domain);
+  const handleRemoveItemFromBlackList = async (
+    domain: string,
+  ): Promise<void> => {
+    const updatedBlackList = blackList.filter(d => d !== domain)
 
-    dispatch(setBlackList(updatedBlackList));
-    await setActualBlackList(updatedBlackList);
+    dispatch(setBlackList(updatedBlackList))
+    await setActualBlackList(updatedBlackList)
   }
-
 
   return {
     handleSetBlackList,
     handleAddItemToBlackList,
     handleRemoveItemFromBlackList,
   }
-};
+}
